fc387791e3ad7b4609b14c0b108fbe9d97932805
Paweł Gardzijewski <91805736+Gardzijewski@users.noreply.github.com>
2026-05-26 11:55:06 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+struct tablica {
+	int liczba_osob;
+	int tab[liczba_osob];
+};
+
+
+int main() {
+	int liczba_osob;
+	std::cin >> liczba_osob;
+	int tab[liczba_osob];
+	return 0;
+}
